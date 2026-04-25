8d3d31742272ef2f12f2aa2f9c68df13f5671485
Lawrence Thomas <lawrence@Lawrences-MacBook-Pro-2.local>
2026-04-25 16:06:52 -0400

    Updates to footer

--- footer.php
+++ footer.php
@@ -39,7 +39,7 @@
           <path d="M19.59 6.69a4.83 4.83 0 01-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 01-2.88 2.5 2.89 2.89 0 01-2.89-2.89 2.89 2.89 0 012.89-2.89c.28 0 .54.04.79.1V9.01a6.33 6.33 0 00-.79-.05 6.34 6.34 0 00-6.34 6.34 6.34 6.34 0 006.34 6.34 6.34 6.34 0 006.33-6.34V8.69a8.18 8.18 0 004.78 1.52V6.75a4.85 4.85 0 01-1.01-.06z"/>
         </svg>
       </a>
-      <a href="#" aria-label="Facebook">
+      <a href="https://www.facebook.com/people/The-Brow-Beast/61558698227616/" aria-label="Facebook">
         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
           <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"/>
         </svg>
@@ -74,6 +74,7 @@
     <div class="footer-links">
       <a href="https://www.instagram.com/thebrowbeast/" target="_blank" rel="noopener noreferrer">Instagram</a>
       <a href="https://www.tiktok.com/@thebrowbeast" target="_blank" rel="noopener noreferrer">TikTok</a>
+       <a href="https://www.facebook.com/people/The-Brow-Beast/61558698227616/" target="_blank" rel="noopener noreferrer">Facebook</a>
       <a href="<?php echo esc_url( get_permalink( get_page_by_path( 'contact' ) ) ?: home_url( '/contact/' ) ); ?>">Contact Us</a>
     </div>
   </div>
